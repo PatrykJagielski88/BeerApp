01709dbfb298d94627ffb125a2ad521918493810
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-09-12 13:46:35 -0500

    Added scaffolding for beer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :beers
+  # get 'companies/index'
+  # get 'companies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/companies', to: 'companies#index', as: 'companies'
+  get '/companies/:id', to: 'companies#show', as: 'company', constraints: { id: /\d+/ }
 end
