03f41f50f8027fd7eab3594d38b90c3b4b5c58b2
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-09-12 20:08:52 -0500

    Successful migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_184232) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_203818) do
   create_table "beers", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "company_id", null: false
+    t.index ["company_id"], name: "index_beers_on_company_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_184232) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "beers", "companies"
 end
